a341bc50d3a89ad4cb12a5615d4ce6e7637747b8
StoneyEagle <StoneyEagle@users.noreply.github.com>
2025-04-15 18:36:54 +0000

    deploy: 15baae2f7b0c5b0c399b949cd0aea173c0705634

--- assets/status-tap-CA9qMXaW.js
+++
@@ -1,3 +0,0 @@
-import{r as i,f as a,b as m,w as p,s as c}from"./MusicPlayer-BjbH7QyS.js";import"./Layout.vue_vue_type_script_setup_true_lang-CUghRGUW.js";import"./index-Dw5gJVRY.js";import"./HomeCorner-NFMlZ_ID.js";import"./GithubCorner-BSUnKm7T.js";import"./logo-BSSc2Qdb.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const T=()=>{const o=window;o.addEventListener("statusTap",()=>{i(()=>{const n=o.innerWidth,r=o.innerHeight,e=document.elementFromPoint(n/2,r/2);if(!e)return;const t=a(e);t&&new Promise(s=>m(t,s)).then(()=>{p(async()=>{t.style.setProperty("--overflow","hidden"),await c(t,300),t.style.removeProperty("--overflow")})})})})};export{T as startStatusTap};
