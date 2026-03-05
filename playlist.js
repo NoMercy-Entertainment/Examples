ac84a72af7ab981513f489186a90ed77d1f0905c
Stoney_Eagle <45034970+StoneyEagle@users.noreply.github.com>
2026-03-05 03:39:04 +0100

    Refactor code structure for improved readability and maintainability

---
+++ playlist.js
@@ -0,0 +1,182 @@
+playlists.set('Jon Shuemaker - Sound Works: Volume 2', [
+{
+  "id": 244782,
+  "cover": "https://api.nomercy.tv/cors?url=https://files.freemusicarchive.org/storage-freemusicarchive-org/album_image%2Fjkp5vrXTA5R0T5QyANjNZFKvSCqdllXoYISLCXmU.jpg",
+  "album_track": [
+    {
+      "name": "Sound Works: Volume 2",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/sound-works-volume-2"
+    }
+  ],
+  "artist_track": [
+    {
+      "name": "Jon Shuemaker",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/"
+    }
+  ],
+  "name": "Soundwork XI",
+  "path": "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/SLrlClVxpebqyO2E2gQXQ9LIU7fVUkAKeShgBwQh.mp3"
+},
+{
+  "id": 244783,
+  "cover": "https://api.nomercy.tv/cors?url=https://files.freemusicarchive.org/storage-freemusicarchive-org/album_image%2Fjkp5vrXTA5R0T5QyANjNZFKvSCqdllXoYISLCXmU.jpg",
+  "album_track": [
+    {
+      "name": "Sound Works: Volume 2",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/sound-works-volume-2"
+    }
+  ],
+  "artist_track": [
+    {
+      "name": "Jon Shuemaker",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/"
+    }
+  ],
+  "name": "Soundwork XII",
+  "path": "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/JCy5KbSFOK9Jdq64kYQ4gQnTjyU0sOPE5cWwUBIF.mp3"
+},
+{
+  "id": 244784,
+  "cover": "https://api.nomercy.tv/cors?url=https://files.freemusicarchive.org/storage-freemusicarchive-org/album_image%2Fjkp5vrXTA5R0T5QyANjNZFKvSCqdllXoYISLCXmU.jpg",
+  "album_track": [
+    {
+      "name": "Sound Works: Volume 2",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/sound-works-volume-2"
+    }
+  ],
+  "artist_track": [
+    {
+      "name": "Jon Shuemaker",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/"
+    }
+  ],
+  "name": "Soundwork XIII",
+  "path": "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/HxwEnm4sO1Qhd6DYTEDcUQGCc5BeA34WoK2bn3n5.mp3"
+},
+{
+  "id": 244785,
+  "cover": "https://api.nomercy.tv/cors?url=https://files.freemusicarchive.org/storage-freemusicarchive-org/album_image%2Fjkp5vrXTA5R0T5QyANjNZFKvSCqdllXoYISLCXmU.jpg",
+  "album_track": [
+    {
+      "name": "Sound Works: Volume 2",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/sound-works-volume-2"
+    }
+  ],
+  "artist_track": [
+    {
+      "name": "Jon Shuemaker",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/"
+    }
+  ],
+  "name": "Soundwork XIV",
+  "path": "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/Z8MfX3eGiW3rd8su2r6e5QK5NnMZL0PKAbqF0BBx.mp3"
+},
+{
+  "id": 244786,
+  "cover": "https://api.nomercy.tv/cors?url=https://files.freemusicarchive.org/storage-freemusicarchive-org/album_image%2Fjkp5vrXTA5R0T5QyANjNZFKvSCqdllXoYISLCXmU.jpg",
+  "album_track": [
+    {
+      "name": "Sound Works: Volume 2",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/sound-works-volume-2"
+    }
+  ],
+  "artist_track": [
+    {
+      "name": "Jon Shuemaker",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/"
+    }
+  ],
+  "name": "Soundwork XV",
+  "path": "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/znEvOnzHpykRmOJpAWdi6WfjfcKbJt6vuKGylQEY.mp3"
+},
+{
+  "id": 244787,
+  "cover": "https://api.nomercy.tv/cors?url=https://files.freemusicarchive.org/storage-freemusicarchive-org/album_image%2Fjkp5vrXTA5R0T5QyANjNZFKvSCqdllXoYISLCXmU.jpg",
+  "album_track": [
+    {
+      "name": "Sound Works: Volume 2",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/sound-works-volume-2"
+    }
+  ],
+  "artist_track": [
+    {
+      "name": "Jon Shuemaker",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/"
+    }
+  ],
+  "name": "Soundwork XVI",
+  "path": "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/vSpN3iecoPmRDWRTc6Xb47X5sD9EppOdJA6iA5sR.mp3"
+},
+{
+  "id": 244788,
+  "cover": "https://api.nomercy.tv/cors?url=https://files.freemusicarchive.org/storage-freemusicarchive-org/album_image%2Fjkp5vrXTA5R0T5QyANjNZFKvSCqdllXoYISLCXmU.jpg",
+  "album_track": [
+    {
+      "name": "Sound Works: Volume 2",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/sound-works-volume-2"
+    }
+  ],
+  "artist_track": [
+    {
+      "name": "Jon Shuemaker",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/"
+    }
+  ],
+  "name": "Soundwork XVII",
+  "path": "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/QRR6KMmgnESGPlzXhknAts3yggTVCqf1i9iEuyH8.mp3"
+},
+{
+  "id": 244789,
+  "cover": "https://api.nomercy.tv/cors?url=https://files.freemusicarchive.org/storage-freemusicarchive-org/album_image%2Fjkp5vrXTA5R0T5QyANjNZFKvSCqdllXoYISLCXmU.jpg",
+  "album_track": [
+    {
+      "name": "Sound Works: Volume 2",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/sound-works-volume-2"
+    }
+  ],
+  "artist_track": [
+    {
+      "name": "Jon Shuemaker",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/"
+    }
+  ],
+  "name": "Soundwork XVIII",
+  "path": "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/67AXJsoKn5xvJnYu1l47e3AFZ1o8lTFrOMJjJgrA.mp3"
+},
+{
+  "id": 244790,
+  "cover": "https://api.nomercy.tv/cors?url=https://files.freemusicarchive.org/storage-freemusicarchive-org/album_image%2Fjkp5vrXTA5R0T5QyANjNZFKvSCqdllXoYISLCXmU.jpg",
+  "album_track": [
+    {
+      "name": "Sound Works: Volume 2",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/sound-works-volume-2"
+    }
+  ],
+  "artist_track": [
+    {
+      "name": "Jon Shuemaker",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/"
+    }
+  ],
+  "name": "Soundwork XIX",
+  "path": "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/gkjSFASIuynsyx5MNjbqibHFmCJ0LZ4Y5HeSxmiU.mp3"
+},
+{
+  "id": 244791,
+  "cover": "https://api.nomercy.tv/cors?url=https://files.freemusicarchive.org/storage-freemusicarchive-org/album_image%2Fjkp5vrXTA5R0T5QyANjNZFKvSCqdllXoYISLCXmU.jpg",
+  "album_track": [
+    {
+      "name": "Sound Works: Volume 2",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/sound-works-volume-2"
+    }
+  ],
+  "artist_track": [
+    {
+      "name": "Jon Shuemaker",
+      "url": "https://freemusicarchive.org/music/jon-shuemaker/"
+    }
+  ],
+  "name": "Soundwork XX",
+  "path": "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/j8pLsLY8xTIj78GrN2b2YnJlTCzGYZ1hId4zqT5d.mp3"
+},
+]);
